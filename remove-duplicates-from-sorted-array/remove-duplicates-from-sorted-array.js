39c7115615daa121ef63afd48c549fe0f5a15244
Rubayat Islam <91696266+Rubayat26@users.noreply.github.com>
2022-04-12 12:32:40 -0400

    Time: 174 ms, Memory: 44 MB - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -4,18 +4,18 @@
  */
 var removeDuplicates = function(nums) {
     
-    for (i=0; i<nums.length-1 ; i++){
-        // if (nums[i]===nums[i+1]){
-        //  nums.splice(i+1, 1)
-        // }
-        for (j=i+1;j<nums.length; j++){
-            while (nums[i]===nums[j]){
-                //console.log(nums[j])
-                nums.splice(j,1);
-                //console.log(nums);
-            }
+    for (i=0; i<nums.length ; i++){
+                                                    // if (nums[i]===nums[i+1]){
+                                                    //  nums.splice(i+1, 1)
+                                                    // }
+                                                    // for (j=i+1;j<nums.length; j++){
+            while (nums[i]===nums[i+1]){
+                                                    //console.log(nums[j])
+                nums.splice(i,1);
+                                                        //console.log(nums);
+                                                    // }
         }
         
     }
-    console.log(nums);
+                                                    // console.log(nums);
 };
