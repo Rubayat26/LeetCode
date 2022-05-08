3cb2240286b35a41adfeea65579ee47a5fbe626d
Rubayat Islam <91696266+Rubayat26@users.noreply.github.com>
2022-05-08 14:30:27 -0400

    Time: 137 ms (32.08%), Space: 44.7 MB (67.71%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.js
@@ -2,23 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var removeDuplicates = function(nums) {
+/*
+var removeDuplicates = function(nums) {
     
-//     for (i=0; i<nums.length ; i++){
-//                                                     // if (nums[i]===nums[i+1]){
-//                                                     //  nums.splice(i+1, 1)
-//                                                     // }
-//                                                     // for (j=i+1;j<nums.length; j++){
-//             while (nums[i]===nums[i+1]){
-//                                                     //console.log(nums[j])
-//                 nums.splice(i,1);
-//                                                         //console.log(nums);
-//                                                     // }
-//         }
+    for (i=0; i<nums.length ; i++){
+                                                  
+                                                  
+            while (nums[i]===nums[i+1]){
+                                                   
+                nums.splice(i,1);
+                                                       
+        }
         
-//     }
-//                                                     // console.log(nums);
-// };
+    }
+*/                                                    
 
 
 var removeDuplicates = function(nums) {
@@ -33,13 +30,14 @@ var removeDuplicates = function(nums) {
             i +=1;
             nums[i]=nums[j]
         }
-       // if (nums[j]===nums[i]){
-       //     //continue
-       // } else {
-       //     i += 1;
-       //     nums[i]=nums[j]
-       // }
-        
+       /* 
+       if (nums[j]===nums[i]){
+           //continue
+       } else {
+           i += 1;
+           nums[i]=nums[j]
+       }
+       */ 
         
 }
 
